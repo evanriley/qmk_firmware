3a600ef0e8f41049479c13b6f29b67c3804e62dc
Evan Riley <me@evanriley.me>
2019-04-04 00:48:00 -0400

    config update

--- keyboards/thevankeyboards/minivan/keymaps/nave/config.h
+++ keyboards/thevankeyboards/minivan/keymaps/nave/config.h
@@ -4,7 +4,19 @@
 #include "../../config.h"
 
 // place overrides here
+
+#undef PERMISSIVE_HOLD
+#undef IGNORE_MOD_TAP_INTERRUPT
+
+#undef RETRO_TAPPING
+#define RETRO_TAPPING
+
+#ifdef TAPPING_TERM
 #undef TAPPING_TERM
-#define TAPPING_TERM 200
+#endif // TAPPING_TERM
+#define TAPPING_TERM 120
+
+
+
 
 #endif
